91fbe5825e5aa19c361841a1e7d3cda104536dbc
nattawat <frank.nattawat@hotmail.com>
2024-01-30 16:44:23 +0700

    fix: error syntax

--- app/components/header.js
+++ app/components/header.js
@@ -3,35 +3,33 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <html lang="en">
-      <body>
-        <div className="w-full  flex justify-around shadow-lg shadow-gray-300">
-          <Image
-            src="/images/get-that-job.png"
-            width={160}
-            height={30}
-            className="p-2 mr-68"
-          />
-          <div className="flex ml-72 ">
-            <div className="mx-2 my-2 p-1">
-              <Image
-                src="/images/signup.png"
-                width={127}
-                height={55}
-                className="hover:bg-[#eabfcd] rounded-xl active:bg-[#F48FB1] "
-              />
-            </div>
-            <div className="mx-2 my-2 p-1">
-              <Image
-                src="/images/login.png"
-                height={40}
-                width={112}
-                className="hover:bg-[#eabfcd] rounded-xl active:bg-[#F48FB1] "
-              />
-            </div>
+    <>
+      <div className="w-full  flex justify-around shadow-lg shadow-gray-300">
+        <Image
+          src="/images/get-that-job.png"
+          width={160}
+          height={30}
+          className="p-2 mr-68"
+        />
+        <div className="flex ml-72 ">
+          <div className="mx-2 my-2 p-1">
+            <Image
+              src="/images/signup.png"
+              width={127}
+              height={55}
+              className="hover:bg-[#eabfcd] rounded-xl active:bg-[#F48FB1] "
+            />
+          </div>
+          <div className="mx-2 my-2 p-1">
+            <Image
+              src="/images/login.png"
+              height={40}
+              width={112}
+              className="hover:bg-[#eabfcd] rounded-xl active:bg-[#F48FB1] "
+            />
           </div>
         </div>
-      </body>
-    </html>
+      </div>
+    </>
   );
 }
