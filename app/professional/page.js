06ed12b27aebfb6314740f0ae26c49fc28a29f4c
du <tb.poopp@gmail.com>
2024-01-31 09:35:05 +0700

    style: edit button category type and salary

--- app/professional/page.js
+++ app/professional/page.js
@@ -99,24 +99,30 @@ export default function Page() {
               <div className="w-[960px] h-[52px] left-0 top-[60px] absolute justify-start items-start gap-4 inline-flex">
                 <div className="flex-col justify-start items-start gap-1 inline-flex">
                   <div
-                    className="text-zinc-600 text-[10px] uppercase tracking-wider"
-                    style={montserrat.style}
+                    className="text-zinc-600 text-[10px] uppercase relative top-[3px] tracking-[.10em]"
+                    style={inter.style}
                   >
                     Category
                   </div>
-                  <select className="category w-[280px] h-[39px] pl-2 border-rose-300 rounded-lg border focus:outline-none">
+                  <img
+                    className="w-[20px] h-[20px] absolute left-[545px] top-[28px] z-10"
+                    src="/images/arrowdown.svg"
+                  />
+                  <select className="category w-[280px] h-[39px] px-2 border-rose-300 rounded-lg border-2 focus:outline-none hover:text-zinc-600 focus:text-neutral-400 focus:ring appearance-none">
                     <option value="" disabled selected>
                       Select a category
                     </option>
-                    <option value="it">IT</option>
-                    <option value="marketing">Marketing</option>
-                    <option value="programmer">Programmer</option>
+                    <option className="text-neutral-700" value="it">
+                      IT
+                    </option>
+                    <option className="text-neutral-700" value="marketing">
+                      Marketing
+                    </option>
+                    <option className="text-neutral-700" value="programmer">
+                      Programmer
+                    </option>
                   </select>
                   <div className="flex-col justify-start items-start gap-1 inline-flex">
-                    <div
-                      className="w-[236px] text-neutral-400 text-sm leading-tight tracking-tight"
-                      style={montserrat.style}
-                    ></div>
                     <div className="w-5 h-5 justify-center items-center flex">
                       <div className="w-5 h-5 relative"></div>
                     </div>
@@ -124,24 +130,30 @@ export default function Page() {
                 </div>
                 <div className="flex-col justify-start items-start gap-1 inline-flex">
                   <div
-                    className="text-zinc-600 text-[10px] uppercase tracking-wider"
-                    style={montserrat.style}
+                    className="text-zinc-600 text-[10px] uppercase relative top-[3px] tracking-[.10em]"
+                    style={inter.style}
                   >
                     Type
                   </div>
-                  <select className="type  w-[280px] h-[39px] pl-2 border-rose-300 rounded-lg border focus:outline-none">
+                  <img
+                    className="w-[20px] h-[20px] absolute right-[690px] top-[28px] z-10"
+                    src="/images/arrowdown.svg"
+                  />
+                  <select className="type  w-[280px] h-[39px] px-2 border-rose-300 rounded-lg border-2 focus:outline-none  hover:text-zinc-600 focus:text-neutral-400 focus:ring appearance-none">
                     <option value="" disabled selected>
                       Select a type
                     </option>
-                    <option value="it">IT</option>
-                    <option value="marketing">Marketing</option>
-                    <option value="programmer">Programmer</option>
+                    <option className="text-neutral-700" value="it">
+                      IT
+                    </option>
+                    <option className="text-neutral-700" value="marketing">
+                      Marketing
+                    </option>
+                    <option className="text-neutral-700" value="programmer">
+                      Programmer
+                    </option>
                   </select>
                   <div className="flex-col justify-start items-start gap-1 inline-flex">
-                    <div
-                      className="w-[236px] text-neutral-400 text-sm leading-tight tracking-tight"
-                      style={montserrat.style}
-                    ></div>
                     <div className="w-5 h-5 justify-center items-center flex">
                       <div className="w-5 h-5 relative"></div>
                     </div>
@@ -150,18 +162,18 @@ export default function Page() {
                 <div className="flex-col justify-start items-start gap-1 inline-flex">
                   <div
                     className="text-zinc-600 text-[10px] uppercase tracking-wider"
-                    style={montserrat.style}
+                    style={inter.style}
                   >
                     Salary Range
                   </div>
                   <div className="justify-start items-center gap-2 inline-flex">
-                    <div className="h-9 p-2 bg-white rounded-lg border border-rose-300 justify-start items-center gap-2 flex">
+                    <div className="h-9 p-2 bg-white rounded-lg border-2 border-rose-300 justify-start items-center gap-2 flex">
                       <div className="w-5 h-5 justify-center items-center flex">
                         <div className="w-5 h-5 relative"></div>
                       </div>
                       <div
                         className="grow shrink basis-0 h-5 text-neutral-400 text-sm leading-tight tracking-tight"
-                        style={montserrat.style}
+                        style={inter.style}
                       >
                         <img
                           className="w-[20px] h-[20px] absolute left-[600px] z-10"
@@ -169,7 +181,7 @@ export default function Page() {
                         />
                         <input
                           type="number"
-                          className="focus:outline-none w-[72px] pl-[10px]"
+                          className="focus:outline-none w-[72px] pl-[10px] text-neutral-700"
                           id="money"
                           name="money"
                           placeholder="min"
@@ -177,21 +189,21 @@ export default function Page() {
                       </div>
                     </div>
                     <div className="w-[15px] h-[0px] rounded-lg border-2 border-neutral-400"></div>
-                    <div className="h-9 p-2 bg-white rounded-lg border border-rose-300 justify-start items-center gap-2 flex">
+                    <div className="h-9 p-2 bg-white rounded-lg border-2 border-rose-300 justify-start items-center gap-2 flex">
                       <div className="w-5 h-5 justify-center items-center flex">
                         <div className="w-5 h-5 relative"></div>
                       </div>
                       <div
                         className="grow shrink basis-0 h-5 text-neutral-400 text-sm leading-tight tracking-tight"
-                        style={montserrat.style}
+                        style={inter.style}
                       >
                         <img
-                          className="w-[20px] h-[20px] absolute left-[745px] z-10"
+                          className="w-[20px] h-[20px] absolute left-[750px] z-10"
                           src="/images/dollar.svg"
                         />
                         <input
                           type="number"
-                          className="focus:outline-none w-[72px] pl-[10px]"
+                          className="focus:outline-none w-[72px] pl-[10px] text-neutral-700"
                           id="money"
                           name="money"
                           placeholder="max"
@@ -203,18 +215,18 @@ export default function Page() {
               </div>
               <div className="w-[419.93px] h-[52px] left-0 top-0 absolute flex-col justify-start items-start gap-1 inline-flex">
                 <div
-                  className="text-zinc-600 text-[10px] uppercase tracking-wider"
-                  style={montserrat.style}
+                  className="text-zinc-600 text-[10px] uppercase tracking-wider relative top-[3px] tracking-[.10em]"
+                  style={inter.style}
                 >
                   search by job title or company name
                 </div>
-                <div className="self-stretch p-2 bg-white rounded-lg border border-rose-300 justify-start items-center gap-2 inline-flex">
+                <div className="self-stretch p-2 bg-white rounded-lg border-2 border-rose-300 justify-start items-center gap-2 inline-flex">
                   <div className="w-5 h-5 justify-center items-center flex">
                     <div className="w-5 h-5 relative"></div>
                   </div>
                   <div
                     className="grow shrink basis-0 h-5 text-neutral-400 text-sm leading-tight tracking-tight"
-                    style={montserrat.style}
+                    style={inter.style}
                   >
                     <img
                       className="w-[20px] h-[20px] absolute left-2 z-10"
@@ -222,7 +234,7 @@ export default function Page() {
                     />
                     <input
                       type="search"
-                      className="w-[390px] h-9 rounded-lg focus:outline-none focus:border-rose-300 justify-start items-start gap-2 inline-flex p-2 relative right-2 bottom-2"
+                      className="w-[389px] h-9 rounded-lg text-neutral-700 focus:outline-none justify-start items-start gap-2 inline-flex p-2 relative right-2 bottom-2"
                       id="site-search"
                       name="search"
                       placeholder="manufacturing, sales, swim"
